c6f8568c92c36603552f4ea3963c095766208c04
Garret Harp <garretcharp@gmail.com>
2019-04-25 11:00:28 -0500

    comit stuff

---
+++ prototype/Grades.swift
@@ -0,0 +1,13 @@
+//
+//  Grades.swift
+//  prototype
+//
+//  Created by Harp, Garret C on 4/25/19.
+//  Copyright © 2019 Qualan Bradford. All rights reserved.
+//
+
+import Foundation
+
+class Grades {
+    
+}
