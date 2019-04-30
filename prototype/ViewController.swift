30fdf4e738feeb7351825b293e9a973e1b312cea
Garret Harp <garretcharp@gmail.com>
2019-04-30 11:43:36 -0500

    fix load class method

--- prototype/ViewController.swift
+++ prototype/ViewController.swift
@@ -96,24 +96,14 @@ class ViewController: UIViewController , WKNavigationDelegate{
     }
     
     func loadClasses(){
-        webView.evaluateJavaScript("function getClassesText(){var length = document.getElementsByClassName(\"AssignmentClass\").length; var string = \"\"; for(var i = 0; i < length; i++){string+=document.getElementsByClassName(\"AssignmentClass\")[i].innerText;} return string;} getClassesText()") { (innerText, error) in
-            let classTable = innerText as? String
+        webView.evaluateJavaScript("function getClassesText(){var length = document.getElementsByClassName(\"AssignmentClass\").length; var arr = []; for(var i = 0; i < length; i++){arr.push(document.getElementsByClassName(\"AssignmentClass\")[i].innerText);} return arr;} getClassesText()") { (innerText, error) in
+            let classTable = innerText as? [String]
             if let classTable = classTable {
-                print(classTable)
+                classTable.forEach({ (ClassItem) in
+                    print(ClassItem)
+                })
             }
         }
-
-        /*webView.evaluateJavaScript("function getClassesText(){var length = document.getElementsByClassName(\"AssignmentClass\").length; return length;} getClassesText()") { (classesTaken, error) in
-            let classes = classesTaken as? Int
-            if let classCount = classes {
-                print(classCount)
-                for number in 0...classCount {
-                    
-                        
-                }
-            }else {
-                print("Is NIL")
-            }*/
             
         }
         
