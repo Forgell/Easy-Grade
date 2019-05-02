5dd94be333547d243cd6cf7fd885f9d814d00b2a
Garret Harp <garretcharp@gmail.com>
2019-05-02 11:24:50 -0500

    Setup login view

---
+++ prototype/LogInViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LogInViewController.swift
+//  prototype
+//
+//  Created by Harp, Garret C on 5/2/19.
+//  Copyright © 2019 Qualan Bradford. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    //@IBOutlet weak var LoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func LoginButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "MainView", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
