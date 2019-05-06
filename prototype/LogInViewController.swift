4cd50153783a2d024d3c40c5335e8790b5dbcd7c
Hardy, Justin E <151710@s020rmc148-11.local>
2019-05-06 11:53:22 -0500

    Fixed bugs involving login from previous commit. Added label to the login screen to represent connection progress. Put in the foundation for the main menu view.

--- prototype/LogInViewController.swift
+++ prototype/LogInViewController.swift
@@ -11,6 +11,8 @@ import WebKit
 
 class LogInViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var notificationLabel: UILabel!
     @IBOutlet weak var Username: UITextField!
     @IBOutlet weak var Password: UITextField!
     //@IBOutlet weak var LoginButton: UIButton!
@@ -23,20 +25,27 @@ class LogInViewController: UIViewController, WKNavigationDelegate {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.navigationDelegate = self
+        notificationLabel.isHidden = true
     }
     
     @IBAction func LoginButtonPressed(_ sender: Any) {
+        pageNumber = 0
+        loginButton.isEnabled = false
+        notificationLabel.textColor = UIColor(displayP3Red: CGFloat(255), green: CGFloat(255), blue: CGFloat(0), alpha: CGFloat(255))
+        notificationLabel.isHidden = false
+        notificationLabel.text = "Establishing connection..."
         let myURL = URL(string:"https://hac.friscoisd.org/HomeAccess/Account/LogOn?ReturnUrl=%2fhomeaccess")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
-        //self.performSegue(withIdentifier: "MainView", sender: self)
     }
     
     var pageNumber = 0
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print("loaded \(pageNumber)")
+        print("loaded page \(pageNumber)")
         switch(pageNumber){
         case 0:
+            notificationLabel.textColor = UIColor(displayP3Red: CGFloat(255), green: CGFloat(255), blue: CGFloat(0), alpha: CGFloat(255))
+            notificationLabel.text = "Attempting Login..."
             webView.evaluateJavaScript("document.getElementById(\"LogOnDetails_UserName\").value = \"" + Username.text! + "\"", completionHandler: nil)
             webView.evaluateJavaScript("document.getElementById(\"LogOnDetails_Password\").value = \"" + Password.text! + "\"", completionHandler: nil)
             
@@ -44,11 +53,18 @@ class LogInViewController: UIViewController, WKNavigationDelegate {
             break
         case 1:
             if webView.url! == URL(string: "https://hac.friscoisd.org/HomeAccess/Home/WeekView")! {
+                notificationLabel.textColor = UIColor(displayP3Red: CGFloat(0), green: CGFloat(255), blue: CGFloat(0), alpha: CGFloat(255))
+                notificationLabel.text = "Login Successful!"
                 print("Login Success!")
                 webView.evaluateJavaScript("window.location = \"https://hac.friscoisd.org/HomeAccess/Content/Student/Assignments.aspx\"", completionHandler: nil)
+                loadClasses()
+                loginButton.isEnabled = true
+                self.performSegue(withIdentifier: "MainView", sender: self)
             }else {
+                notificationLabel.textColor = UIColor(displayP3Red: CGFloat(255), green: CGFloat(0), blue: CGFloat(0), alpha: CGFloat(255))
+                notificationLabel.text = "There was an error logging in."
                 print("Error logging in")
-                self.performSegue(withIdentifier: "Login", sender: nil)
+                loginButton.isEnabled = true
             }
         default:
             print("Hello")
@@ -61,6 +77,17 @@ class LogInViewController: UIViewController, WKNavigationDelegate {
             let vc = segue.destination as! ViewController
             vc.username = Username.text!
             vc.password = Password.text!
+        }
+    }
+    
+    func loadClasses(){
+        webView.evaluateJavaScript("function getClassesText(){var length = document.getElementsByClassName(\"AssignmentClass\").length; var arr = []; for(var i = 0; i < length; i++){arr.push(document.getElementsByClassName(\"AssignmentClass\")[i].innerText);} return arr;} getClassesText()") { (innerText, error) in
+            let classTable = innerText as? [String]
+            if let classTable = classTable {
+                classTable.forEach({ (ClassItem) in
+                    print(ClassItem)
+                })
+            }
         }
     }
     
