3e4f0bfc61be3a3aa8b67f9ee4cd1243b2079ae9
Garret Harp <garretcharp@gmail.com>
2019-05-16 11:47:37 -0500

    Update main handler of view controller

--- prototype/ViewController.swift
+++ prototype/ViewController.swift
@@ -12,16 +12,17 @@ import WebKit
 
 class ViewController: UIViewController , WKNavigationDelegate{
     
-    var viewFromLogin: WKWebView?
-
-    @IBOutlet var webView: WKWebView!
+    var webView: WKWebView?
     var pageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let view = webView {
+            loadClasses(view: view)
+        } else {
+            self.performSegue(withIdentifier: "LoginPage", sender: nil)
+        }
         
-        webView = viewFromLogin
-        loadClasses()
     }
     
     
@@ -30,8 +31,8 @@ class ViewController: UIViewController , WKNavigationDelegate{
         super.didReceiveMemoryWarning()
     }
     
-    func loadClasses(){
-        webView.evaluateJavaScript("function getClassesText(){var length = document.getElementsByClassName(\"sg-legacy-iframe\")[0].contentDocument.getElementsByClassName(\"AssignmentClass\").length; var arr = []; for(var i = 0; i < length; i++){arr.push(document.getElementsByClassName(\"sg-legacy-iframe\")[0].contentDocument.getElementsByClassName(\"AssignmentClass\")[i].innerText);} return arr;} getClassesText()") { (innerText, error) in
+    func loadClasses(view: WKWebView){
+        view.evaluateJavaScript("function getClassesText(){var length = document.getElementsByClassName(\"sg-legacy-iframe\")[0].contentDocument.getElementsByClassName(\"AssignmentClass\").length; var arr = []; for(var i = 0; i < length; i++){arr.push(document.getElementsByClassName(\"sg-legacy-iframe\")[0].contentDocument.getElementsByClassName(\"AssignmentClass\")[i].innerText);} return arr;} getClassesText()") { (innerText, error) in
                 let classTable = innerText as? [String]
                 if let classTable = classTable {
                     classTable.forEach({ (ClassItem) in
