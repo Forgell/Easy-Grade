37978bcfb78be82ba7fc2b1e441d0ed8e0b7c8db
Hardy, Justin E <151710@s020rmc148-11.local>
2019-05-20 11:15:17 -0500

    Small adjustment, added back ViewController

---
+++ prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prototype
+//
+//  Created by Hardy, Justin E on 5/20/19.
+//  Copyright © 2019 Qualan Bradford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // This UIViewController class is no longer in use.
+    
+}
